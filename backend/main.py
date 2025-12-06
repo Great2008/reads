78f388b341a46bf9af40364e0f9e629496f68d4a
Graphitech <aakorokaragreatname@gmail.com>
2025-12-06 01:53:35 +0100

    Update CORS settings to allow all origins and methods

--- backend/main.py
+++ backend/main.py
@@ -30,9 +30,9 @@ CORS
 
 app.add_middleware(
 CORSMiddleware,
-allow_origins=[""],
+allow_origins=["*"],
 allow_credentials=True,
-allow_methods=[""],
+allow_methods=["*"],
 allow_headers=["*"],
 )
 
